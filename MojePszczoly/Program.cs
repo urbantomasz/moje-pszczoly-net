e7d0906a87281249baef80c8aeb79bfcf90d22ad
Tomasz Urban <urbantomasz94@gmail.com>
2025-03-18 12:28:05 +0100

    remove db connection string change sqlite to mysql

--- MojePszczoly/Program.cs
+++ MojePszczoly/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddAuthorization(options =>
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
