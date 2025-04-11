289fd123eeb42ea58139e053d27f6e997e043023
Tomasz Urban <urbantomasz94@gmail.com>
2025-04-11 12:18:18 +0200

    dates change

--- MojePszczoly/Services/OrderService.cs
+++ MojePszczoly/Services/OrderService.cs
@@ -82,7 +82,7 @@ namespace MojePszczoly.Services
         {
             if (!_cache.TryGetValue("orders", out List<OrderDto> orders))
             {
-                var dates = _dateService.GetCurrentWeekDates();
+                var dates = _dateService.GetUpcomingDates();
 
                 var orderEntities = await _context.Orders
                     .Include(o => o.Items)
